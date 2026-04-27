855fd74e1f45f5e4a70e025ba5a3a53b4639dcab
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-27 17:51:47 -0400

    Atualiza src/app/screens/today/today.ts

---
+++ src/app/screens/today/today.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-today',
+  imports: [],
+  templateUrl: './today.html',
+  styleUrl: './today.css',
+})
+export class Today {}
