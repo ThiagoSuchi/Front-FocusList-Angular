db88ad644badc2aaeead4a9ca598d25cacf64744
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-28 17:27:08 -0400

    style(menu): configura host element p/ 100% de altura

--- src/app/components/shared/menu/menu.ts
+++ src/app/components/shared/menu/menu.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { MenuLink } from '../menu-link/menu-link';
 import { Title } from "../title/title";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
-  imports: [MenuLink, Title],
+  imports: [MenuLink, Title, RouterLink],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
+  host: {
+    class: 'block h-full'
+  }
 })
 export class Menu {}
