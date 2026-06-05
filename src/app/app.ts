1ae8e7bdca7e8664846b8a1ca0259d0ab1c57cd7
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-05 14:51:16 -0400

    fix: Correção de imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Page } from './components/shared/page/page';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Page],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
   standalone: true
