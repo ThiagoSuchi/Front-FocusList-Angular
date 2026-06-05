cbe1ac92c7e6467e71d4581334082f45c04280c4
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-04 20:50:44 -0400

    feat: rotas integradas de login e rcadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
-import { Today } from './screens/today/today';
+import { Login } from './screens/login/login';
 
 export const routes: Routes = [
     {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./screens/register/register').then(r => r.Register)
+    },
+    {
         path: '',
-        component: Today
+        loadComponent: () => import('./screens/today/today').then(t => t.Today)
     },
     {
         path: 'my-profile',
