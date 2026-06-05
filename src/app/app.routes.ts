848e23355204766545e45ce917352b5bb6d84826
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-05 14:51:03 -0400

    chore: Ajustando rotas com sidebar e rotas sem sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,17 @@ import { Routes } from '@angular/router';
 import { Login } from './screens/login/login';
 
 export const routes: Routes = [
-    {
-        path: 'login',
-        component: Login
-    },
-    {
-        path: 'register',
-        loadComponent: () => import('./screens/register/register').then(r => r.Register)
-    },
+    // Rotas sem sidebar
+    { path: 'login', component: Login },
+    { path: 'register', loadComponent: () => import('./screens/register/register').then(r => r.Register) },
+
+    // Rotas com sidebar
     {
         path: '',
-        loadComponent: () => import('./screens/today/today').then(t => t.Today)
+        loadComponent: () => import('./components/shared/page/page').then(m => m.Page),
+        children: [
+            { path: '', loadComponent: () => import('./screens/today/today').then(m => m.Today) },
+            { path: 'my-profile', loadComponent: () => import('./screens/my-profile/my-profile').then(m => m.MyProfile) }
+        ]
     },
-    {
-        path: 'my-profile',
-        loadComponent: () => import('./screens/my-profile/my-profile').then(m => m.MyProfile)
-    }
 ];
