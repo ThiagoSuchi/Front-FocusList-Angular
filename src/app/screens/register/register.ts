4f7de58042848ee54d275a7f0b34caf8a85c324a
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-12 18:58:55 -0400

    feat: configurar imports e reactive form de cadastro com validadores

--- src/app/screens/register/register.ts
+++ src/app/screens/register/register.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DesignAuth } from '../../components/shared/design-auth/design-auth';
+import { RouterLink } from "@angular/router";
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators, type AbstractControl } from '@angular/forms';
+import { ValidationError } from '../../components/shared/validators/validationError';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [DesignAuth, RouterLink, JsonPipe, ReactiveFormsModule, ValidationError],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  public readonly viewPassword = signal(false);
+
+  protected form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [null, [
+        Validators.required,
+        Validators.minLength(3)
+      ]],
+      email: [null, [
+        Validators.required,
+        Validators.email 
+      ]],
+      password: [null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/)
+      ]],
+      confirmPassword: [null]
+    }, { validators: this.passwordMatchValidator })
+  }
+
+}
