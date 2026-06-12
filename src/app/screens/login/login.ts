abd04d3b6f4b625ce67299c76a8d3467a6db3815
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-12 18:58:54 -0400

    feat: configurar reactive form de login com validadores

--- src/app/screens/login/login.ts
+++ src/app/screens/login/login.ts
@@ -1,15 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { DesignAuth } from '../../components/shared/design-auth/design-auth';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, DesignAuth],
+  imports: [RouterLink, DesignAuth, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  //
+  // fazer DI do LoginService
+  //
   public viewPassword = signal(false);
+
+  protected form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required]]
+    })
+  }
 
   togglePassword() {
     this.viewPassword.update(v => !v);
