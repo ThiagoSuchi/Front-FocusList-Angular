7b3c4331a74cf9bd3cbc74770b834268306abc1b
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-23 16:48:47 -0400

    chore: Verifica se o usuário possui autorização

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,20 +1,28 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   let authService = inject(AuthService);
+
   const token = authService.getToken();
 
-  if (!token) {
-    return next(req);
-  }
+  const cloneReq = token 
+  ? req.clone ({
+      setHeaders: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+  : req;
 
-  const cloneReq = req.clone({
-    setHeaders: {
-      Authorization: 'Bearer ' + token
-    }
-  });
-  
-  return next(cloneReq);
+  return next(cloneReq).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        authService.logout();
+      }
+
+      return throwError(() => error)
+    })
+  )
 };
