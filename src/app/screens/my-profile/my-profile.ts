5bb7bc649a9c18e30979b4a55bb21efce8e95cae
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-06-25 17:22:36 -0400

    feat: Configurar método get

--- src/app/screens/my-profile/my-profile.ts
+++ src/app/screens/my-profile/my-profile.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Title } from "../../components/shared/title/title";
 import { Footer } from '../../components/shared/footer/footer';
+import { UserService } from '../../core/services/user.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-profile',
-  imports: [Title, Footer],
+  imports: [Title, Footer, ReactiveFormsModule],
   templateUrl: './my-profile.html',
   styleUrl: './my-profile.css',
   standalone: true
 })
-export class MyProfile {}
+export class MyProfile implements OnInit {
+  private readonly userService = inject(UserService);
+
+  protected form!: FormGroup;
+
+  ngOnInit(): void {
+    this.userService.getUser();
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [this.show()],
+      email: [this.show()]
+    });
+  }
+  
+  protected show() {
+    this.userService.getUser().subscribe(user => {
+      this.form.patchValue({
+        name: user.name,
+        email: user.email
+      });
+    });
+  }
+}
