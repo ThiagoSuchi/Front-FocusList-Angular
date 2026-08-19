6c4203dc4287c1828bab0927bb6455d5cd546466
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-08-19 17:37:50 -0400

    feat: adicionar estado do usuário no perfil

--- src/app/screens/my-profile/my-profile.ts
+++ src/app/screens/my-profile/my-profile.ts
@@ -3,6 +3,7 @@ import { Title } from "../../components/shared/title/title";
 import { Footer } from '../../components/shared/footer/footer';
 import { UserService } from '../../core/services/user.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IUserProfileDTO } from '../../models/user.model';
 
 @Component({
   selector: 'app-my-profile',
@@ -15,24 +16,43 @@ export class MyProfile implements OnInit {
   private readonly userService = inject(UserService);
 
   protected form!: FormGroup;
+  protected user: IUserProfileDTO | null = null;
 
   ngOnInit(): void {
-    this.userService.getUser();
-  }
-
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      name: [this.show()],
-      email: [this.show()]
-    });
-  }
-  
-  protected show() {
     this.userService.getUser().subscribe(user => {
+      this.user = user
+
       this.form.patchValue({
         name: user.name,
         email: user.email
       });
     });
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [this.ngOnInit()],
+      email: [this.ngOnInit()]
+    });
+  }  
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+
+    this.userService.uploadProfileImage(file)
+      .subscribe({
+        next: (res) => {
+          this.user!.profileImageUrl = res.profileImageUrl
+        } ,
+        error: (error) => {
+          console.error('Erro ao enviar imagem: ', error);
+        }
+      })
   }
 }
